abac341e9955e2a47b49d41c07226863803a8e40
AlexandreLadriere <alexandre.ladriere77@gmail.com>
2022-04-14 21:38:02 +0200

    add: CalendarService

--- app/src/main/java/com/alexandreladriere/f1companion/CalendarMediumWidget.kt
+++ app/src/main/java/com/alexandreladriere/f1companion/CalendarMediumWidget.kt
@@ -1,23 +1,46 @@
 package com.alexandreladriere.f1companion
 
+import android.app.AlarmManager
+import android.app.PendingIntent
 import android.appwidget.AppWidgetManager
 import android.appwidget.AppWidgetProvider
 import android.content.Context
+import android.content.Intent
+import android.os.SystemClock
+import android.util.Log
 import android.widget.RemoteViews
+import android.widget.Toast
+
 
 /**
  * Implementation of App Widget functionality.
  */
 class CalendarMediumWidget : AppWidgetProvider() {
+
+    private var service: PendingIntent? = null
+
     override fun onUpdate(
         context: Context,
         appWidgetManager: AppWidgetManager,
         appWidgetIds: IntArray
     ) {
-        // There may be multiple widgets active, so update all of them
-        for (appWidgetId in appWidgetIds) {
-            updateAppWidget(context, appWidgetManager, appWidgetId)
+        val manager = context.getSystemService(Context.ALARM_SERVICE) as AlarmManager
+        val i = Intent(context, CalendarService::class.java)
+
+        if (service == null) {
+            service = PendingIntent.getService(context, 0, i, PendingIntent.FLAG_CANCEL_CURRENT)
         }
+        manager.setRepeating(
+            AlarmManager.ELAPSED_REALTIME,
+            SystemClock.elapsedRealtime(),
+            3600000,
+            service
+        )
+        Log.d("UpdatingWidget: ","onUpdate");
+        // There may be multiple widgets active, so update all of them
+        // for (appWidgetId in appWidgetIds) {
+        //     updateAppWidget(context, appWidgetManager, appWidgetId)
+        // }
     }
 
     override fun onEnabled(context: Context) {
@@ -34,6 +57,7 @@ internal fun updateAppWidget(
     appWidgetManager: AppWidgetManager,
     appWidgetId: Int
 ) {
+    Toast.makeText(context, CURRENT_SEASON_ENDPOINT, Toast.LENGTH_LONG).show()
     val widgetText = context.getString(R.string.appwidget_text)
     // Construct the RemoteViews object
     val views = RemoteViews(context.packageName, R.layout.calendar_medium_widget)
