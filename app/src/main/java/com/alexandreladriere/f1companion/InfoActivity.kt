5c1cd03df9b0df71cf1742c4e5a2b82c94439195
AlexandreLadriere <alexandre.ladriere77@gmail.com>
2022-05-01 15:33:19 +0200

    add: version num in info activity

--- app/src/main/java/com/alexandreladriere/f1companion/InfoActivity.kt
+++ app/src/main/java/com/alexandreladriere/f1companion/InfoActivity.kt
@@ -1,7 +1,11 @@
 package com.alexandreladriere.f1companion
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.pm.PackageManager
 import android.os.Bundle
+import android.view.View
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+
 
 class InfoActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -10,5 +14,18 @@ class InfoActivity : AppCompatActivity() {
 
         // Get a support ActionBar corresponding to this toolbar and enable the Up button
         supportActionBar?.setDisplayHomeAsUpEnabled(true)
+        setVersion()
+    }
+
+    fun setVersion() {
+        var versionName = "Version "
+        try {
+            val packageInfo = packageManager.getPackageInfo(packageName, 0)
+            versionName += packageInfo.versionName
+        } catch (e: PackageManager.NameNotFoundException) {
+            e.printStackTrace()
+        }
+        val tv = findViewById<View>(R.id.textview_info_activity_app_version) as TextView
+        tv.text = versionName
     }
 }
